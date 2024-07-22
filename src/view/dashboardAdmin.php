f6d4d3f8ca47264264e0b07a35a61f6f52b60a7e
Lucian Laffont <lucianlaffont31@gmail.com>
2024-07-22 02:18:20 +0200

    Page
    Ajout :
    Temporaire - Dashboard client
    Temporaire - dashboard Admin

---
+++ src/view/dashboardAdmin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue sur votre espace Administrateur</title>
+</head>
+<body>
+    <div>Menu</div>
+    <div>Dashboard</div>
+    <div>Carte</div>
+    <div>Paperasse</div>
+</body>
+</html>
---
+++ src/view/dashboardClient.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=                                                            , initial-scale=1.0">
+    <title>Bienvenue sur votre espace client</title>
+</head>
+<body>
+    <div>Menu</div>
+    <div>Dashboard</div>
+    <div>Carte</div>
+    <div>Paperasse</div>
+</body>
+</html>
