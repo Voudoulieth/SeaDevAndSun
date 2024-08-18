721a64e6a8cfd3de903730b905218a89554df589
Lucian Laffont <lucianlaffont31@gmail.com>
2024-08-18 22:29:59 +0200

    page de connexion front end

--- src/view/loggin.php
+++ src/view/loggin.php
@@ -25,22 +25,37 @@ if (!empty($_POST)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Votre page de connexion"/>
+    <link rel="stylesheet" href="/Css/login.css">
     <title>Connexion</title>
 </head>
 <body>
-    <div>
-        <form action="#" method="post">
-            <div>
-            <input type="text" name="email" placeholder="Votre email">
+    <div class="main">
+        <div class="main_screen">
+            <div class="main_screen_title">
+                <h1>SEA DEV AND SUN</h1>
             </div>
-            <div>
-            <input type="password" name="password" placeholder="Mot de passe">
-            </div>
-            <button type="submit">Se connecter</button>
-        </form>
-        
-
-        <?= var_dump($_SESSION)?>
+            <form action="#" method="post">
+                <div class="input-group">
+                    <label for="email" class="hidden">Email :</label>
+                    <input type="email" name="email" placeholder="Votre email" required>
+                </div>
+                <div class="input-group">
+                    <label for="password" class="hidden">Mot de passe :</label>
+                    <input type="password" name="password" placeholder="Mot de passe" required>
+                </div>
+                <div class="remember">
+                    <input type="checkbox" class="remember-me" name="remember"/>
+                    <label for="remember" class="remember-me">se souvenir de moi</label>
+                </div>
+                
+                <button type="submit">Se connecter</button>
+            </form>
+            
+        </div>
     </div>
+    <footer>
+        <p>Designé par Alexandre Clament | Créé par Sea Dev and Sun</p>
+    </footer>
 </body>
 </html>
